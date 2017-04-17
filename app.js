e22429cd9dadd3715a7334db92a616e7fdd6a74a
Stefan Vischer <stefan.vischer@lemon42.com>
2017-04-17 11:53:48 +0200

    setup app

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ const
 const
   app = express();
   port = config.get('server.port');
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.listen(port, () =>{
+  console.log(`Server is up on port ${port}`);
+});
